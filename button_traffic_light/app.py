a62cf86941d652731aac3e82064a229712cb1766
Francisco Valerio <trainwerck1979@gmail.com>
2024-07-11 14:33:38 -0600

    trying to create my first heroku app! notice that since i'm using flask, I had to change to a deployment based on a buildpack

--- button_traffic_light/app.py
+++ button_traffic_light/app.py
@@ -1,16 +1,48 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import Flask, render_template, request, redirect, url_for, Response
+from flask_socketio import SocketIO, emit
+from functools import wraps
+import eventlet
+import eventlet.wsgi
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 
 # Dictionary to keep track of students' statuses
 students_status = {}
 
+# Simple authentication
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'password'
+
+def check_auth(username, password):
+    """Check if a username / password combination is valid."""
+    return username == ADMIN_USERNAME and password == ADMIN_PASSWORD
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/admin')
+@requires_auth
+def admin():
+    return render_template('admin.html', students_status=students_status)
 
 @socketio.on('join')
 def on_join(data):
